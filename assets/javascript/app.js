dd1370733be54f3f909f044a4d90bd841fbc124d
JamesBaine <jamesmbaine@gmail.com>
2017-06-04 22:21:16 -0700

    did same to giphy url

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,7 @@ var gifs = ["rick ross", "migos", "tyler the creator", "dj khaled", "future","li
     function displayGifInfo() {
       $('#gif-input').text('');
       var person = $(this).attr("data-person");
-      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         person + "&api_key=dc6zaTOxFJmzC&limit=10";
       $("#gif-view").empty();
 
